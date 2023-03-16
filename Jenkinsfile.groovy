0b91d058fbe051df3b5e0d039c6872567f2e3096
cristocabrera3 <cfcabrerat@hotmail.com>
2023-03-16 00:44:35 -0500

    a

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
         AWS_REGION = 'us-east-1'
         GITHUB_REPO_URL = 'https://github.com/cristocabrera3/jenkins-lambda.git'
         STACK_NAME = 'my-stack'
-        BUCKET_NAME = "my-bucket-${env.BUILD_ID}".toLowerCase()
+        BUCKET_NAME = 'jenkinsdemobucket16032013'
     }
 
     stages {
