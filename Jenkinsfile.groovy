93915d023c422b24bcc244dee193bfc60c67e89a
cristocabrera3 <cfcabrerat@hotmail.com>
2023-03-15 22:03:51 -0500

    Zip commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,8 +5,6 @@ pipeline {
 
     environment {
         AWS_REGION = 'us-east-1'
-        AWS_ACCESS_KEY_ID = 'AKIA2HIGE43Y5BCJCNUP'
-        AWS_SECRET_ACCESS_KEY = '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'
         GITHUB_REPO_URL = 'https://github.com/cristocabrera3/jenkins-lambda.git'
         STACK_NAME = 'my-stack'
     }
@@ -14,15 +12,20 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'master', url: 'https://github.com/cristocabrera3/jenkins-lambda.git'
+                git branch: 'master', url: env.GITHUB_REPO_URL
             }
         }
-
-        stage('Build & Deploy') {
+        stage('Build') {
             steps {
+                bat 'zip -r python.zip .'
+            }
+        }
+        stage('Deploy') {
+            withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'cloud_user']]) {
                 bat '"C:\\Program Files\\Amazon\\AWSCLIV2\\aws" cloudformation deploy --region %AWS_REGION% --template-file template.yaml --stack-name %STACK_NAME%'
             }
         }
+        
 
         stage('Test') {
             steps {
