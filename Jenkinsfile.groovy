9c6a4a5beb24d7a7e837965cc554a10fae502e59
cristocabrera3 <cfcabrerat@hotmail.com>
2023-03-16 01:16:52 -0500

    Final final commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
         AWS_REGION = 'us-east-1'
         GITHUB_REPO_URL = 'https://github.com/cristocabrera3/jenkins-lambda.git'
-        STACK_NAME = 'my-other-stack'
+        STACK_NAME = 'my-stack'
         BUCKET_NAME = 'myuniquebucket16032026'
     }
 
@@ -14,6 +14,16 @@ pipeline {
                 git branch: 'master', url: env.GITHUB_REPO_URL
             }
         }
+        
+        stage('Build') {
+            steps {
+                bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "mkdir python"'
+                bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "mv lambda_function.py python/lambda_function.py"'
+                zip zipFile: 'python.zip', archive: false, dir: 'python'
+                archiveArtifacts artifacts: 'python.zip', fingerprint: true
+            }
+        }
+
         stage('Create Bucket') {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'cloud_user']]) {
@@ -24,19 +34,11 @@ pipeline {
         stage('Upload to S3') {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'cloud_user']]) {
-                    bat '"C:\\Program Files\\Amazon\\AWSCLIV2\\aws" s3 cp lambda_function.py s3://%BUCKET_NAME%/'
+                    bat '"C:\\Program Files\\Amazon\\AWSCLIV2\\aws" s3 cp python.zip s3://%BUCKET_NAME%/'
                 }
             }
         }
 
-        // stage('Build') {
-        //     steps {
-        //         bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "mkdir python"'
-        //         bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "mv lambda_function.py python/lambda_function.py"'
-        //         zip zipFile: 'python.zip', archive: false, dir: 'python'
-        //         archiveArtifacts artifacts: 'python.zip', fingerprint: true
-        //     }
-        // }
         stage('Deploy') {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'cloud_user']]) {
